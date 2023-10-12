a0d4bd7eee530ae9c1947a58fc898f181887c4c5
JuukenNoHimeKasra <70822036+JuukenNoHimeKasra@users.noreply.github.com>
2023-10-12 16:41:57 -0700

    starting point

--- Decked Out/Assets/Scripts/Enemy.cs
+++ Decked Out/Assets/Scripts/Enemy.cs
@@ -6,15 +6,41 @@ public class Enemy : MonoBehaviour
 {
 
     public float moveSpeed = 2.0f; 
-    public Transform targetTower; 
+    public Transform targetCastle;
+    public GameObject enemyPrefab;
+    public Transform[] spawnPoints;
+
+    private float timeSinceLastSpawn = 0.0f;
+    public float spawnInterval = 3.0f;
 
     private void Update()
     {
-        if (targetTower != null)
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (timeSinceLastSpawn >= spawnInterval)
+        {
+            SpawnEnemy();
+            timeSinceLastSpawn = 0.0f;
+        }
+        if (targetCastle != null)
         {
   
-            Vector3 moveDirection = (targetTower.position - transform.position).normalized;
+            Vector3 moveDirection = (targetCastle.position - transform.position).normalized;
             transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+            
         }
+    }
+    void SpawnEnemy()
+    {
+        if (spawnPoints.Length == 0)
+        {
+            return;
+        }
+
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+        Transform spawnPoint = spawnPoints[randomIndex];
+
+        Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+        
     }
 }
