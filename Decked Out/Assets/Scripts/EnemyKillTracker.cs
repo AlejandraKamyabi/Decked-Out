8b3ea142a68ca674777239c3edf73bd1937901f4
ChseBrws <chaseburrows444@gmail.com>
2023-11-27 21:27:51 -0800

    Gems/Souls

--- Decked Out/Assets/Scripts/EnemyKillTracker.cs
+++ Decked Out/Assets/Scripts/EnemyKillTracker.cs
@@ -6,12 +6,16 @@ using TMPro;
 public class EnemyKillTracker : MonoBehaviour
 {
     public TextMeshProUGUI enemyCountText;
+    public TextMeshProUGUI gemCountText;
     public TextMeshProUGUI wave;
     public TextMeshProUGUI endGameEnemyCountText;
+    public TextMeshProUGUI endGameGemCountText;
     public TextMeshProUGUI endGameWave;
     public int totalEnemiesDestroyed = 0;
+    public int totalGemsCollected = 0;
     public int currentWave = 1;
     public float duration;
+    public float gemDropChance = 0.01f;
     private GameLoader _loader;
     private WaveManager mouse;
 
@@ -20,9 +24,11 @@ public class EnemyKillTracker : MonoBehaviour
         _loader = ServiceLocator.Get<GameLoader>();
         _loader.CallOnComplete(Initialize);
     }
+
     private void Initialize()
     {
         UpdateEnemyCountText();
+        UpdateGemCountText();
         mouse = ServiceLocator.Get<WaveManager>();
     }
 
@@ -30,18 +36,39 @@ public class EnemyKillTracker : MonoBehaviour
     {
         totalEnemiesDestroyed++;
         UpdateEnemyCountText();
+
+        // Check for gem drop
+        if (Random.value <= gemDropChance)
+        {
+            CollectGem();
+        }
     }
+
+    private void CollectGem()
+    {
+        totalGemsCollected++;
+        UpdateGemCountText();
+    }
+
     public void WaveUpdate()
     {
         currentWave++;
         UpdateEnemyCountText();
-
     }
+
     public void resetWave()
     {
         currentWave = 1;
         UpdateEnemyCountText();
+    }
 
+    public void ResetValues()
+    {
+        totalEnemiesDestroyed = 0;
+        // Do not reset gems here
+        currentWave = 1;
+        UpdateEnemyCountText();
+        UpdateGemCountText();
     }
 
     private void UpdateEnemyCountText()
@@ -52,16 +79,24 @@ public class EnemyKillTracker : MonoBehaviour
             if (totalEnemiesDestroyed > 0)
             {
                 StartCoroutine(ChangeTextColour(duration));
-            }            
+            }
         }
         wave.text = "Wave: " + currentWave.ToString();
     }
-    
-    IEnumerator ChangeTextColour(float duraton)
+
+    private void UpdateGemCountText()
+    {
+        if (gemCountText != null)
+        {
+            gemCountText.text = "Gems: " + totalGemsCollected.ToString();
+        }
+    }
+
+    IEnumerator ChangeTextColour(float duration)
     {
         enemyCountText.color = Color.red;
 
-        yield return new WaitForSeconds(duraton);
+        yield return new WaitForSeconds(duration);
 
         enemyCountText.color = Color.white;
     }
@@ -69,8 +104,12 @@ public class EnemyKillTracker : MonoBehaviour
     public void EndGame()
     {
         endGameEnemyCountText.text = "Kills: " + totalEnemiesDestroyed.ToString();
+        endGameGemCountText.text = "Gems: " + totalGemsCollected.ToString();
         endGameWave.text = "Wave: " + currentWave.ToString();
+
+        // Reset values when the game ends
+        // You may choose to reset other things, but not gems
+        ResetValues();
     }
 }
 
-  
