844912f321efd7fb889fd695f8b2412894b55bfd
Galalendon <galalendon@gmail.com>
2024-01-29 18:22:38 -0800

    Jiggle Added to Heart

---
+++ Decked Out/Assets/01_Scripts/Towers_Scripts/Castle.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Castle : MonoBehaviour
+{
+    public float maxHealth = 100.0f;
+    private float currentHealth;
+
+    public Slider healthSlider;
+    public EndGameSplashManager endGame;
+    private GameLoader _loader;
+    private WaveManager wave;
+
+    HeartJiggle _heartJiggle;
+
+    public float health { get { return currentHealth; } }
+
+    private void Start()
+    {
+        _loader = ServiceLocator.Get<GameLoader>();
+        _loader.CallOnComplete(Initialize);
+    }
+    public void Initialize()
+    {
+        _heartJiggle = FindObjectOfType<HeartJiggle>();
+        currentHealth = maxHealth;
+        healthSlider.maxValue = maxHealth;
+        wave = ServiceLocator.Get<WaveManager>();
+        UpdateHealthUI();
+    }
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        UpdateHealthUI();
+        _heartJiggle.StartJiggle(health);
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        endGame.Death();
+        wave.StopWave();
+
+    }
+    public void ResetHealth()
+    {
+        currentHealth = 100;
+        UpdateHealthUI();        
+    }
+    private void UpdateHealthUI()
+    {
+
+        healthSlider.value = currentHealth;
+    }
+}
