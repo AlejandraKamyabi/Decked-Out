e14910f09d5f9c3f8c859cc290d7aac58f90c763
david0617 <davidyu18800@gmail.com>
2024-04-09 15:31:44 -0700

    AudioManager

---
+++ Decked Out/Assets/01_Scripts/Managers and Handlers/AudioManager.cs
@@ -0,0 +1,181 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource SFXSource;
+
+    private GameLoader _loader;
+    public enum SFXSound
+    {
+        f
+    }
+    public enum musicSound
+    {
+        f
+    }
+    private Dictionary<SFXSound, AudioClip> SFXSoundAudioClipDictionary;
+    private Dictionary<musicSound, AudioClip> musicSoundAudioClipDictionary;
+
+    /*protected void Awake()
+    {
+        SFXSoundAudioClipDictionary = new Dictionary<SFXSound, AudioClip>();
+        foreach (SFXSound sound in System.Enum.GetValues(typeof(SFXSound)))
+        {
+            SFXSoundAudioClipDictionary[sound] = Resources.Load<AudioClip>(sound.ToString());
+        }
+
+        musicSoundAudioClipDictionary = new Dictionary<musicSound, AudioClip>();
+        foreach (musicSound sound in System.Enum.GetValues(typeof(musicSound)))
+        {
+            musicSoundAudioClipDictionary[sound] = Resources.Load<AudioClip>(sound.ToString());
+        }
+    }
+    private void Start()
+    {
+        _loader = ServiceLocator.Get<GameLoader>();
+        _loader.CallOnComplete(Initialize);
+
+        if (PlayerPrefs.HasKey("masterVolume") || PlayerPrefs.HasKey("musicVolume") || PlayerPrefs.HasKey("SFXVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            setMasterVolume();
+            setMusicVolume();
+            setSFXVolume();
+        }
+    }*/
+    private void Awake()
+    {
+        SFXSoundAudioClipDictionary = new Dictionary<SFXSound, AudioClip>();
+        foreach (SFXSound sound in System.Enum.GetValues(typeof(SFXSound)))
+        {
+            SFXSoundAudioClipDictionary[sound] = Resources.Load<AudioClip>(sound.ToString());
+        }
+
+        musicSoundAudioClipDictionary = new Dictionary<musicSound, AudioClip>();
+        foreach (musicSound sound in System.Enum.GetValues(typeof(musicSound)))
+        {
+            musicSoundAudioClipDictionary[sound] = Resources.Load<AudioClip>(sound.ToString());
+        }
+    }
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("masterVolume") || PlayerPrefs.HasKey("musicVolume") || PlayerPrefs.HasKey("SFXVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            setMasterVolume();
+            setMusicVolume();
+            setSFXVolume();
+        }
+        //_loader = ServiceLocator.Get<GameLoader>();
+        //_loader.CallOnComplete(Initialize);
+    }
+    public void setMasterVolume()
+    {
+        float volume = masterSlider.value;
+        myMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("masterVolume", volume);
+    }
+    public void setMusicVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+    public void setSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+    private void LoadVolume()
+    {
+        if (PlayerPrefs.HasKey("masterVolume"))
+        {
+            masterSlider.value = PlayerPrefs.GetFloat("masterVolume");
+            setMasterVolume();
+        }
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+            setMusicVolume();
+        }
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+            setSFXVolume();
+        }
+    }
+    public void playMusicClip(musicSound sound)
+    {
+        musicSource.clip = musicSoundAudioClipDictionary[sound];
+        musicSource.Play();
+    }
+    public void stopMusicClip()
+    {
+        musicSource.Stop();
+    }
+    public void playSFXClip(SFXSound sound)
+    {
+        SFXSource.PlayOneShot(SFXSoundAudioClipDictionary[sound]);
+    }
+    public void playSFXClip(SFXSound sound, AudioSource source)
+    {
+        SFXSource.clip = SFXSoundAudioClipDictionary[sound];
+        SFXSource.Play();
+    }
+    public void StopAllAudio()
+    {
+        AudioSource[] allAudioSources = FindObjectsOfType<AudioSource>() as AudioSource[];
+
+        foreach (var audioS in allAudioSources)
+        {
+            if (!audioS.loop)
+            {
+                audioS.Stop();
+            }
+        }
+    }
+
+    public void Initialize()
+    {
+        SFXSoundAudioClipDictionary = new Dictionary<SFXSound, AudioClip>();
+        foreach (SFXSound sound in System.Enum.GetValues(typeof(SFXSound)))
+        {
+            SFXSoundAudioClipDictionary[sound] = Resources.Load<AudioClip>(sound.ToString());
+        }
+
+        musicSoundAudioClipDictionary = new Dictionary<musicSound, AudioClip>();
+        foreach (musicSound sound in System.Enum.GetValues(typeof(musicSound)))
+        {
+            musicSoundAudioClipDictionary[sound] = Resources.Load<AudioClip>(sound.ToString());
+        }
+
+        if (PlayerPrefs.HasKey("masterVolume") || PlayerPrefs.HasKey("musicVolume") || PlayerPrefs.HasKey("SFXVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            setMasterVolume();
+            setMusicVolume();
+            setSFXVolume();
+        }
+    }
+}
